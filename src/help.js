42a7b97fee2ae4b06c70216a253c9989a4076696
zuckzinonnichan007 <77843721+zuckzinonnichan007@users.noreply.github.com>
2021-01-23 14:48:16 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,149 @@
+const help = (prefix) => { 
+	return `                 
+
+               𝗭𝗨𝗖𝗞𝗭𝗜𝗡 𝗕𝗢𝗧 𝗩-𝗧𝗘𝗦𝗧𝗘
+ <════════════════════>
+
+                        𝗩-𝗧𝗘𝗦𝗧𝗘.
+                       
+                       𝗠𝗜𝗡𝗜-𝗜𝗡𝗙𝗢
+          ======================
+          •𝗗𝗢𝗡𝗢: 𝗭𝗨𝗖𝗞𝗭𝗜𝗡•
+          •𝗡𝗨𝗠𝗕𝗘𝗥: wa.me/556392942637 •
+          •𝗖𝗔𝗡𝗔𝗟: 𝗢𝗙𝗙•
+          •𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗡: 𝗢𝗙𝗙•
+          •𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞: 𝗢𝗙𝗙•
+          •𝗔𝗥157𝗕𝗢𝗧: 𝗦𝗘𝗨 𝗚𝗢𝗦𝗧𝗢𝗦𝗢
+     
+
+            ●──●𝗠𝗘𝗡𝗨 𝗗𝗢 𝗕𝗢𝗧●──●
+               𝗩-𝗧𝗘𝗦𝗧𝗘: 𝗩𝗘𝗥𝗦𝗔𝗢 𝗧𝗘𝗦𝗧𝗘
+
+
+   𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑩𝑶𝑻 𝒁𝑼𝑪𝑲𝒁𝑰𝑵
+✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓✓
+
+      𝑬𝑿𝑪𝑳𝑼𝑺𝑰𝑽𝑶 𝑷𝑨𝑹𝑨 𝑨𝑫𝑴'𝑺
+
+
+  ${prefix}kick @menciona o membro
+   --> 𝗕𝗔𝗡𝗜𝗥 𝗠𝗘𝗠𝗕𝗥𝗢
+
+   ${prefix}add +55xxxxxxxxx
+  --> 𝗔𝗗𝗗 𝗔𝗟𝗚𝗨𝗘𝗠
+
+  ${prefix}listadmin
+  --> 𝗠𝗔𝗥𝗖𝗔𝗥 𝗔𝗗𝗠'𝗦
+ 
+  ${prefix}welcome [1/0]
+  --> 𝗔𝗧𝗜𝗩𝗔𝗥 𝗢𝗨 𝗗𝗘𝗦𝗔𝗧𝗜𝗩𝗔𝗥 𝗕𝗩
+ 
+  ${prefix}group tutup
+  --> 𝗙𝗘𝗖𝗛𝗔𝗥 𝗚𝗥𝗨𝗣𝗢
+ 
+  ${prefix}group buka
+  --> 𝗔𝗕𝗥𝗜𝗥 𝗚𝗥𝗨𝗣𝗢
+ 
+  ${prefix}demote @marca adm
+ --> 𝗥𝗘𝗠𝗢𝗩𝗘𝗥 𝗔𝗗𝗠
+ 
+  ${prefix}promote @marca membro
+ --> 𝗣𝗥𝗢𝗠𝗢𝗩𝗘𝗥 𝗔𝗗𝗠
+ 
+  ${prefix}nsfw [1/0]
+  --> 𝗔𝗧𝗜𝗩𝗔𝗥 𝗢𝗨 𝗗𝗘𝗦𝗔𝗧𝗜𝗩𝗔𝗥 𝗟𝗢𝗟𝗜
+ 
+  ${prefix}simih [1/0]
+  --> 𝗡 𝗥𝗘𝗖𝗢𝗠𝗘𝗡𝗗𝗢!!!!!
+
+
+
+                 𝑴𝑰𝑫𝑰𝑨 𝑬 𝑭𝑰𝑮𝑼𝑹𝑨
+
+
+  ${prefix}sticker + foto/Gif
+ --> 𝗙𝗔𝗭𝗘𝗥 𝗙𝗜𝗚𝗨𝗥𝗔
+
+  ${prefix}dogzin
+ --> 𝗗𝗢𝗚 𝗙𝗢𝗙𝗜𝗡𝗛𝗢
+
+  ${prefix}text3d seu txt
+ --> 𝗙𝗢𝗧𝗢 𝗖𝗠 𝗦𝗘𝗨 𝗧𝗫𝗧
+
+  ${prefix}pokemon
+ --> 𝗣𝗢𝗞𝗘𝗠𝗢𝗡 𝗔𝗟𝗘𝗔𝗧𝗢𝗥𝗜𝗢
+
+  ${prefix}ocr (marque uma foto)
+ --> 𝗧𝗫𝗧𝗦 𝗗𝗔 𝗙𝗢𝗧𝗢
+ 
+  ${prefix}tts pt seu txt
+ --> 𝗧𝗘𝗫𝗧𝗢 𝗘𝗠 𝗔𝗨𝗗𝗜𝗢
+ 
+   ${prefix}toimg (marca uma fig)
+ --> 𝗙𝗜𝗚 𝗘𝗠 𝗙𝗢𝗧𝗢
+ 
+  ${prefix}ytmp4 (link de algum vídeo)
+ --> 𝗜𝗡𝗙𝗢 𝗩𝗜𝗗𝗘𝗢
+
+  ${prefix}hilih seu txt
+ --> converter todas vogais do
+  txt para "i"
+
+  ${prefix}quotes
+ --> 𝗧𝗫𝗧 𝗠𝗢𝗧𝗜𝗩𝗔𝗖𝗔𝗢
+
+  ${prefix}map 00000-000
+ --> pega imagem do local
+  desejado 
+
+  ${prefix}nsfwloli
+ --> 𝗙𝗢𝗧𝗢 𝗗𝗘 𝗟𝗢𝗟𝗜 𝗡𝗦𝗙𝗪
+ 
+  ${prefix}loli
+ --> 𝗙𝗢𝗧𝗢 𝗟𝗢𝗟𝗜
+ 
+  ${prefix}fototiktok
+ --> 𝗙𝗢𝗧𝗢 𝗧𝗜𝗞 𝗧𝗢𝗞
+
+  ${prefix}phlogo seu|txt
+ --> 𝗟𝗢𝗚𝗢 𝗣𝗢𝗥𝗡 𝗛𝗨𝗕
+ 
+  ${prefix}tiktokstalk
+ --> ${prefix}tiktokstalk e username da pessoa
+ 
+ ══════════════════════
+                    𝑫𝑶𝑵𝑶 𝑫𝑶 𝑩𝑶𝑻
+ 
+
+
+  ${prefix}clearall
+ --> 𝗟𝗜𝗠𝗣𝗔𝗥 𝗪𝗛𝗔𝗧𝗦
+
+  ${prefix}block
+ --> 𝗕𝗟𝗢𝗖𝗞 𝗘𝗠 𝗠𝗘𝗠𝗕𝗥𝗢
+ 
+  ${prefix}unblock
+ -->𝗗𝗘𝗦𝗕𝗟𝗢𝗤𝗨𝗘𝗔 𝗠𝗘𝗠𝗕𝗥𝗜
+
+  ${prefix}bc
+ --> 𝗧𝗥𝗔𝗦𝗠𝗜𝗦𝗦𝗔𝗢
+ 
+  ${prefix}leave
+ --> 𝗦𝗘𝗜 𝗟𝗔
+
+  ${prefix}clone @membro
+ -->𝗖𝗟𝗢𝗡𝗔𝗥 𝗙𝗢𝗧𝗢 𝗗𝗔 𝗣𝗘𝗦𝗦𝗢𝗔 𝗠𝗔𝗥𝗖𝗔𝗗𝗔
+ 
+  ${prefix}setpp
+ --> 𝗠𝗨𝗗𝗔 𝗙𝗢𝗧𝗢 𝗚𝗥𝗨𝗣𝗢
+ 
+  ${prefix}setprefix
+ --> 𝗠𝗨𝗗𝗔𝗥 𝗢 𝗣𝗥𝗘𝗙𝗜𝗫𝗢
+
+
+𝗭𝗨𝗖𝗞𝗭𝗜𝗡 𝗕𝗢𝗧𝗦 𝗩-𝗧𝗘𝗦𝗧𝗘© 2021
+
+`
+}
+exports.help = help
+
